5724bdfef61e15dd71ab6fd262bbbfab0ce81fb5
Eggroll <Eggroll@DESKTOP-9V4Q3LQ.surewest.net>
2018-10-24 20:28:16 -0700

    Added name

--- src/Hello.java
+++ src/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 		// Add a new line with your information
 		System.out.println("216237711 - This line was added by Setsuna Kai Chiu");
 		System.out.println("217744957 - This line was added by Jordan Angeles.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by Brendon Wilson.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 
