32d9a33afd5b7330062185579928e19d7a7ade3f
Jordan <Jordan@10.0.0.68>
2018-10-24 20:00:31 -0700

    Added my name

--- src/Hello.java
+++ src/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("216237711 - This line was added by Setsuna Kai Chiu");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("217744957 - This line was added by Jordan Angeles.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
