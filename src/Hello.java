1574fddb2dbfb24177d0e9009f023debd0212ebb
shiki-p <kaiyizhao@csus.edu>
2018-10-24 19:36:55 -0700

    first

--- src/Hello.java
+++ src/Hello.java
@@ -1,19 +1,13 @@
 public class Hello {
-
 	public static void main(String[] args) {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
-		System.out.println("216237711 - Setsuna Chiu (Kai).");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("216237711 - This line was added by Setsuna Kai Chiu");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 
 	}
-
 }
-
